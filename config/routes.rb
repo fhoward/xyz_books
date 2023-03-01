2549bd1eb0f802b99e1ca32a700b62f1a5514580
fsusa <fsusa@iripple.com>
2023-03-01 23:33:14 +0800

    initial

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  get '/search', to: 'home#search'
   get '/books', to: 'books#index'
   get '/books/:isbn_13', to: 'books#show' 
   get 'convert_isbn', to: 'books#convert_isbn'
