a5161e750d0007f14c862bd312fa8ff568ec9e1a
fsusa <fsusa@iripple.com>
2023-03-01 19:01:07 +0800

    add author spec and validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,4 +8,7 @@ class Book < ApplicationRecord
  
   validates :title,:isbn_13,:price,:publication_year,:publisher_id, presence: true
   validates_presence_of :authors, message: "must have at least one author"
+  validates_uniqueness_of :title,:isbn_13,case_sensitive: false
+
+  attr_accessor :isbn_10
 end
